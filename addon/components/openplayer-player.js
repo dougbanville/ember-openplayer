20929e65f1b75bd8d9a93d452959349c24325928
dougbanville <doug.banville@rte.ie>
2019-06-09 16:09:35 +0100

    added rangetouch to smmoth out the slider experience

--- addon/components/openplayer-player.js
+++ addon/components/openplayer-player.js
@@ -2,31 +2,56 @@ import Component from "@ember/component";
 import layout from "../templates/components/openplayer-player";
 import OpenPlayer from "openplayerjs";
 import { inject as service } from "@ember/service";
+import RangeTouch from "rangetouch";
 
 export default Component.extend({
   layout,
 
   openplayerPlayer: service(),
 
+  sliding: false,
+
   didInsertElement() {
     var player = new OpenPlayer("openplayer-player", null, false, {});
+    const range = new RangeTouch(".e-range", {});
+    console.log(range);
     // Don't forget to start the player
     player.init();
 
     this.openplayerPlayer.setPlayer(player);
+    this.htmlPlayer = this.openplayerPlayer.player.getElement();
 
     this.progress = document.getElementById("progress");
     this.progressBar = document.getElementById("progress-bar");
+    this.progressRange = document.getElementById("progress_range");
 
+    /*
     this.progress.addEventListener("click", e => {
       var pos = (e.pageX - this.progress.offsetLeft) / this.progress.offsetWidth;
       var htmlPlayer = this.openplayerPlayer.player.getElement();
       htmlPlayer.currentTime = pos * htmlPlayer.duration;
     });
+    */
+    /*
+    this.progressRange.addEventListener("change", e => {
+      this.htmlPlayer.currentTime = this.progressRange.value;
+    });
+    */
+
+    this.progressRange.addEventListener("input", e => {
+      if (!this.sliding) {
+        this.htmlPlayer.currentTime = this.progressRange.value;
+      }
+    });
 
     this.openplayerPlayer.player.getElement().addEventListener("loadedmetadata", player => {
       //set intial duration but this not reliable on some devices
       this.progress.setAttribute("max", player.srcElement.duration);
+      this.progressRange.setAttribute("max", player.srcElement.duration);
+      this.progressRange.setAttribute("value", player.srcElement.currentTime);
+      this.progressRange.setAttribute("step", 0.1);
+      this.progressRange.setAttribute("min", 0);
+
       this.openplayerPlayer.setDuration(player.srcElement.duration);
     });
 
@@ -41,6 +66,13 @@ export default Component.extend({
     this.openplayerPlayer.player.getElement().addEventListener("timeupdate", player => {
       this.openplayerPlayer.setTime(player.srcElement.currentTime);
       this.progress.setAttribute("max", player.srcElement.duration);
+      this.progressRange.setAttribute("max", player.srcElement.duration);
+      this.progressRange.setAttribute("min", 0);
+
+      //this.progressRange.setAttribute("value", player.srcElement.currentTime);
+
+      this.progressRange.value = player.srcElement.currentTime;
+
       this.openplayerPlayer.setDuration(player.srcElement.duration);
 
       this.progress.value = player.srcElement.currentTime;
